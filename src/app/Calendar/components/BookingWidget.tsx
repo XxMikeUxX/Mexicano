0f9860818451a07fa1b84eb176be693367719e10
XxMikeUxX <mikeu1807@gmail.com>
2026-02-14 13:17:45 +0100

    reserva

--- src/app/Calendar/components/BookingWidget.tsx
+++ src/app/Calendar/components/BookingWidget.tsx
@@ -24,7 +24,7 @@ export const BookingWidget = () => {
 
     // ¡ENVÍA REAL!
     try {
-        const res = await fetch("/api/reserva", {
+        const res = await fetch("/api/reservas", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
